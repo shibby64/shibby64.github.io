421696a6d468b2653a375ec6e5f1cf7939e00015
shibby64 <ethan.harris@live.com>
2025-05-30 22:42:37 -0700

    fonts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,12 @@ import React from "react";
 const Home: React.FC = () => {
   return (
     <>
-      <div className="text-title">Welcome to my site!</div>
+      <section>
+        <div className="text-title"></div>
+      </section>
       <section className="content-box" id="explanation">
-        <div className="text-section-header header">Why am I here? What is this about?</div>
-        <div className="text-body body"></div>
+        <div className="text-section-header header">Welcome to my site!</div>
+        <div className="text-body body">There isn't much here yet, but feel free to check out my blog!</div>
       </section>
     </>
   );
